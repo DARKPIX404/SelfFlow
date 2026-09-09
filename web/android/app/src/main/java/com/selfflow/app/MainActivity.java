c75232bdcf8f28c6ebab2d7fb4089dc74d8f6fa3
Kimi Code <kimi@localhost>
2026-09-09 12:18:54 +0300

    feat(update): in-app обновление APK с GitHub Releases
    - нативный плагин SelfFlowUpdater: getAppInfo/download/install (FileProvider)
    - авто-проверка при старте, кастомный диалог с прогрессом скачивания
    - секция «Обновление» в настройках с ручной проверкой
    - CI встраивает VITE_RELEASE_NUM (run_number) в бандл для сравнения с тегом релиза

--- web/android/app/src/main/java/com/selfflow/app/MainActivity.java
+++ web/android/app/src/main/java/com/selfflow/app/MainActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
 import com.selfflow.app.alarm.AlarmOverlayPlugin;
+import com.selfflow.app.UpdaterPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(AlarmOverlayPlugin.class);
+        registerPlugin(UpdaterPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
